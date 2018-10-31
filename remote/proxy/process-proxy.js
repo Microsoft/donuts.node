c58fdb3ad91c2469aa0174af4c0adfd59453e12e
Steven Shan <stevshan@microsoft.com>
2018-10-31 14:57:55 -0700

    save.

--- remote/proxy/process-proxy.js
+++ remote/proxy/process-proxy.js
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License. See License file under the project root for license information.
 //-----------------------------------------------------------------------------
+'use strict';
 
 const utils = require("donuts.node/utils");
 const { ChannelProxy } = require("./channel-proxy");

e18d0ba6f57a1e50b4b7cefd3c0898bbf68f3de2
Steven Shan <stevshan@microsoft.com>
2019-06-03 13:11:34 -0700

    save

--- remote/types.remote.d.ts
+++ remote/types.remote.d.ts
@@ -20,4 +20,26 @@ namespace Donuts.Remote {
         getPropertyAsync<T>(refId: string, property: string | number): Promise<T>;
         setPropertyAsync(refId: string, property: string | number, value: any): Promise<boolean>;
     }
+
+    interface IMessageTransmitter<TRequest, TResponse> {
+        sendAsync(request: TRequest): Promise<TResponse>;
+
+        // on(event: "message-sending", handler: (request: TRequest) => TRequest);
+        // on(event: "message-sent", handler: (request: TRequest) => void);
+        on(event: "message-received", handler: (response: TResponse) => TResponse);
+        off(event: "message-received", handler: (response: TResponse) => TResponse);
+        once(event: "message-received", handler: (response: TResponse) => TResponse);
+    }
+
+    interface TMessageProcessor<TInMessage, TOutMessage> {
+        handleAsync(msg: TInMessage): Promise<TOutMessage>;
+    }
+
+    interface ICommunicator<TOutgoingMessage, TRequest, TResponse, TIncomingMessage> {
+        sendMessageAsync(msg: TOutgoingMessage): Promise<TIncomingMessage>;
+
+        outgoingMessageProcessors: Array<IMessageProcessor<TOutgoingMessage, TRequest>>;
+        incomingMessageProcessors: Array<IMessageProcessor<TResponse, TIncomingMessage>>;
+        messageTransmitter: IMessageTransmitter<TRequest, TResponse>;
+    }
 }
